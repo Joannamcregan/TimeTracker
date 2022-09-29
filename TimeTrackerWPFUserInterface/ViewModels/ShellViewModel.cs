a09ed2745bf0c462c71316d2fb5107fa87bc0ac6
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2022-09-29 13:20:21 -0400

    Add WPF project with MVVM
    Adds our WPF interface project and configures it with Caliburn micro for our MVVM framework
    
    Also includes the simple container DI system

---
+++ TimeTrackerWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTrackerWPFUserInterface.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
